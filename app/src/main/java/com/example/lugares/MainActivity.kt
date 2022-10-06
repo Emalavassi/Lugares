64ee40a43ee6359dc62e3e4d365b3501bda620a5
Emmanuel Malavassi <malavassiemmanuel@gmail.com>
2022-10-05 20:33:23 -0600

    Commit Final Primera Clase

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.example.lugares.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    public fun actualiza(user: FirebaseUser) {
+        if (user != null) {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart(){
+        super.onStart()
+        val usuario = auth.currentUser
+        if (usuario != null) {
+            actualiza(usuario)
+        }
+    }
+
 }
